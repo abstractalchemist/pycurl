7bfe56e9006dfd4ba52a029359e2c8f933966d8d
Jason Hirata <jhirata@integrity-apps.com>
2018-09-24 12:12:18 -1000

    fixing shebang

--- pycurl.py
+++ pycurl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 
 from httplib import HTTPConnection
 import sys
@@ -7,8 +7,21 @@ import os.path
 import json
 import pprint
 
+# Configuration Variables
+# API_PORT      - the port that the api is listening on
+# API_HOST      - the host that the api is listening on
+# METHOD        - the httpd method to use
+# DATA          - a path to a file to use as the body for the http request
+# BODY          - a string to send as the body for the http request
+# URL           - the portal rest url to place the request.  It should be the path after /portal/delegate/rest/$URL
+# PORTAL_USER   - the portal user to send the request as.  By default, it uses the running user
+
+
+
 if __name__ == '__main__':
-   connection = HTTPConnection('localhost',9000)
+   host = os.environ['API_HOST'] if 'API_HOST' in os.environ else 'localhost'
+   port = int(os.environ['API_PORT']) if 'API_PORT' in os.environ else 9000
+   connection = HTTPConnection(host, port)
    headers = { 'unix-name': ( os.environ['PORTAL_USER'] if 'PORTAL_USER' in os.environ else os.environ['USER']), 'application':'test' }
    if 'DATA' in os.environ:
       with open(os.environ['DATA']) as f:
